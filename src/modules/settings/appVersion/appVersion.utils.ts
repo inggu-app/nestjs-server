8213643ad5316553d57563720e8419e043ef71bf
Movsar Khalakhoev <movsar.kh.128@gmail.com>
2021-06-27 20:08:02 +0300

    Исправлена ошибка с проверкой версии приложения

--- src/modules/settings/appVersion/appVersion.utils.ts
+++ src/modules/settings/appVersion/appVersion.utils.ts
@@ -1,22 +1,22 @@
-export function checkAppVersion(currentVersion: string, verifiableVersion: string): boolean {
-  const lastVersionRelease = verifiableVersion.split('.').map(x => +x)
+export function checkAppVersion(verifiableVersion: string, currentVersion: string): boolean {
+  const verifiableVersionRelease = verifiableVersion.split('.').map(x => +x)
   const currentVersionRelease = currentVersion.split('.').map(x => +x)
 
-  const isMajor = lastVersionRelease[0] > currentVersionRelease[0]
-  const isMinor = lastVersionRelease[1] > currentVersionRelease[1]
-  const isPatch = lastVersionRelease[2] > currentVersionRelease[2]
+  const isMajor = verifiableVersionRelease[0] > currentVersionRelease[0]
+  const isMinor = verifiableVersionRelease[1] > currentVersionRelease[1]
+  const isPatch = verifiableVersionRelease[2] > currentVersionRelease[2]
 
   if (isMajor) {
     return true
   }
 
-  if (isMinor && lastVersionRelease[0] >= currentVersionRelease[0]) {
+  if (isMinor && verifiableVersionRelease[0] >= currentVersionRelease[0]) {
     return true
   }
 
   return (
     isPatch &&
-    lastVersionRelease[0] >= currentVersionRelease[0] &&
-    lastVersionRelease[1] >= currentVersionRelease[1]
+    verifiableVersionRelease[0] >= currentVersionRelease[0] &&
+    verifiableVersionRelease[1] >= currentVersionRelease[1]
   )
 }
