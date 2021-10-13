40b6dc380bad2a26924543a0593edb3c196fe1d4
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-10-13 17:30:07 +0300

    Убраны лишние импорты

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,19 +3,11 @@ import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { AdminModel } from './admin.model'
-import { ConfigModule, ConfigService } from '@nestjs/config'
 import { OwnerJwtStrategy } from '../../global/strategies/ownerJwt.strategy'
-import { JwtModule } from '@nestjs/jwt'
-import getJWTConfig from '../../configs/jwt.config'
 import { AdminJwtStrategy } from '../../global/strategies/adminJwt.strategy'
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getJWTConfig,
-    }),
     TypegooseModule.forFeature([
       {
         typegooseClass: AdminModel,
--- src/modules/responsible/responsible.module.ts
+++ src/modules/responsible/responsible.module.ts
@@ -2,9 +2,6 @@ import { forwardRef, Module } from '@nestjs/common'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { ResponsibleModel } from './responsible.model'
 import { ResponsibleService } from './responsible.service'
-import { JwtModule } from '@nestjs/jwt'
-import getJWTConfig from '../../configs/jwt.config'
-import { ConfigModule, ConfigService } from '@nestjs/config'
 import { ResponsibleController } from './responsible.controller'
 import { GroupModule } from '../group/group.module'
 import { ResponsibleJwtStrategy } from '../../global/strategies/responsibleJwt.strategy'
@@ -12,13 +9,7 @@ import { FacultyModule } from '../faculty/faculty.module'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     forwardRef(() => FacultyModule),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getJWTConfig,
-    }),
     TypegooseModule.forFeature([
       {
         typegooseClass: ResponsibleModel,
