c83d75be1bfacad4ad95d6d8d957c064dc19621d
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-10-28 10:05:30 +0300

    Установлен Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: ['http://localhost:3001', 'http://localhost:5000'] })
+  const config = new DocumentBuilder().setTitle('IngGU API').setVersion('1.0').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   await app.listen(3000)
 }
 bootstrap()
