7aec883c26962dbbb5b25dee40278e360fc7a3a4
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-02-08 12:15:45 +0300

    Небольшие изменения

--- src/modules/settings/secretLabel/secretLabel.controller.ts
+++ src/modules/settings/secretLabel/secretLabel.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 import { CreateSecretLabelDto } from './dto/createSecretLabel.dto'
 import { SecretLabelService } from './secretLabel.service'
-import { ParseDatePipe } from '../../../global/pipes/date.pipe'
 import { ApiTags } from '@nestjs/swagger'
+import { DateQueryParam } from '../../../global/decorators/DateQueryParam.decorator'
 
 @ApiTags('Секретная надпись')
 @Controller()
@@ -18,7 +18,7 @@ export class SecretLabelController {
   }
 
   @Get('/')
-  async getSecretLabel(@Query('updatedAt', new ParseDatePipe({ required: false })) updatedAt?: Date) {
+  async getSecretLabel(@DateQueryParam('updatedAt', { required: false }) updatedAt?: Date) {
     const secretLabel = await this.secretLabelService.getActiveSecretLabel()
 
     if ((secretLabel && secretLabel?.updatedAt && updatedAt && updatedAt < secretLabel?.updatedAt) || !updatedAt) {
--- src/modules/settings/semesterRange/semesterRange.controller.ts
+++ src/modules/settings/semesterRange/semesterRange.controller.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Body, Controller, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
+import { BadRequestException, Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 import { SemesterRangeService } from './semesterRange.service'
 import { CreateSemesterRangeDto } from './dto/createSemesterRange.dto'
-import { ParseDatePipe } from '../../../global/pipes/date.pipe'
 import { SEMESTER_RANGE_DATE_INCORRECT_RANGE } from '../../../global/constants/errors.constants'
 import { AdminUserAuth } from '../../../global/decorators/AdminUserAuth.decorator'
+import { DateQueryParam } from '../../../global/decorators/DateQueryParam.decorator'
 
 @Controller()
 export class SemesterRangeController {
@@ -22,7 +22,7 @@ export class SemesterRangeController {
   }
 
   @Get('/')
-  async getSemesterStartTime(@Query('updatedAt', new ParseDatePipe({ required: false })) updatedAt?: Date) {
+  async getSemesterStartTime(@DateQueryParam('updatedAt', { required: false }) updatedAt?: Date) {
     const semesterStartTime = await this.semesterStartDateService.getActiveSemesterStartDate()
 
     if ((semesterStartTime && semesterStartTime?.updatedAt && updatedAt && updatedAt < semesterStartTime?.updatedAt) || !updatedAt) {
