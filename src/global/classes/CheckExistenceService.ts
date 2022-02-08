479b6f90582ac0b84692e19e295caff22820f4b7
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-02-08 12:15:11 +0300

    Фильтр проверки существования улучшен

--- src/global/classes/CheckExistenceService.ts
+++ src/global/classes/CheckExistenceService.ts
@@ -1,6 +1,7 @@
 import { ModelType } from '@typegoose/typegoose/lib/types'
-import { Error } from 'mongoose'
+import { Error, FilterQuery } from 'mongoose'
 import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { DocumentType } from '@typegoose/typegoose'
 
 interface CheckWithOneFilterOptions<T> {
   error?: string | Error | ((filter: T) => string | Error)
@@ -17,25 +18,25 @@ export class CheckExistenceService<Model = any> {
     private readonly defaultMessageForNotExisting?: string | ((arg: Model) => string)
   ) {}
 
-  async throwIfExists<T extends Partial<Model>>(filter: T, options?: CheckWithOneFilterOptions<T>): Promise<void>
-  async throwIfExists<T extends Partial<Model>>(filter: T[], options?: CheckWithManyFiltersOptions<T>): Promise<void>
-  async throwIfExists<T extends Partial<Model>>(
+  async throwIfExists<T extends FilterQuery<DocumentType<Model>>>(filter: T, options?: CheckWithOneFilterOptions<T>): Promise<void>
+  async throwIfExists<T extends FilterQuery<DocumentType<Model>>>(filter: T[], options?: CheckWithManyFiltersOptions<T>): Promise<void>
+  async throwIfExists<T extends FilterQuery<DocumentType<Model>>>(
     filter: T | T[],
     options?: CheckWithOneFilterOptions<T> | CheckWithManyFiltersOptions<T>
   ): Promise<void> {
     return this.check(filter, async f => await this.model.exists(f), BadRequestException, this.defaultMessageForExisting, options)
   }
 
-  async throwIfNotExists<T extends Partial<Model>>(filter: T, options?: CheckWithOneFilterOptions<T>): Promise<void>
-  async throwIfNotExists<T extends Partial<Model>>(filter: T[], options?: CheckWithManyFiltersOptions<T>): Promise<void>
-  async throwIfNotExists<T extends Partial<Model>>(
+  async throwIfNotExists<T extends FilterQuery<DocumentType<Model>>>(filter: T, options?: CheckWithOneFilterOptions<T>): Promise<void>
+  async throwIfNotExists<T extends FilterQuery<DocumentType<Model>>>(filter: T[], options?: CheckWithManyFiltersOptions<T>): Promise<void>
+  async throwIfNotExists<T extends FilterQuery<DocumentType<Model>>>(
     filter: T | T[],
     options?: CheckWithOneFilterOptions<T> | CheckWithManyFiltersOptions<T>
   ): Promise<void> {
     return this.check(filter, async f => !(await this.model.exists(f)), NotFoundException, this.defaultMessageForNotExisting, options)
   }
 
-  private async check<T extends Partial<Model>>(
+  private async check<T extends FilterQuery<DocumentType<Model>>>(
     filter: T | T[],
     condition: (filter: T) => Promise<boolean>,
     error: typeof BadRequestException | typeof NotFoundException,
@@ -45,7 +46,7 @@ export class CheckExistenceService<Model = any> {
     const castedFilter = Array.isArray(filter) ? filter : [filter]
 
     for await (const f of castedFilter) {
-      if (await condition(f as T)) {
+      if (await condition(f)) {
         if (options?.error) {
           if (typeof options.error === 'function') {
             if (typeof options.error(f) === 'string') throw new error(options.error(f))
