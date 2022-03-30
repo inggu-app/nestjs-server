5b03f3f3eea9d06824a0c7df8179b488d467d751
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-03-31 01:41:38 +0300

    Улучшен декоратор валидации и преобразования mongoId; реализована валидация доступности для создания расписания занятий

--- src/global/decorators/IsMongoIdWithTransform.decorator.ts
+++ src/global/decorators/IsMongoIdWithTransform.decorator.ts
@@ -1,37 +1,40 @@
-import { IsDefined, isMongoId, ValidationOptions } from 'class-validator'
+import { buildMessage, IsDefined, isMongoId, registerDecorator, ValidationOptions } from 'class-validator'
 import { Transform, TransformFnParams } from 'class-transformer'
 import { Types } from 'mongoose'
-import { BadRequestException } from '@nestjs/common'
 
-function transformMongoIdWithParams(
-  params: TransformFnParams,
-  validationOptions?: ValidationOptions
-): Types.ObjectId | Types.ObjectId[] | undefined {
-  if (validationOptions?.each) {
-    if (!Array.isArray(params.value)) throw new BadRequestException(`${params.key} must be an array`)
-
-    const errors: string[] = []
-    const result: Types.ObjectId[] = []
-    params.value.forEach((item, index) => {
-      if (isMongoId(item)) result.push(Types.ObjectId(item))
-      else errors.push(`${params.key}.${index} must be a mongodb id`)
-    })
-
-    if (errors.length) {
-      throw new BadRequestException(errors)
-    } else {
-      return result
-    }
+function transformMongoIdWithParams(params: TransformFnParams): Types.ObjectId | Types.ObjectId[] | undefined {
+  if (Array.isArray(params.value)) {
+    if (!params.value.reduce((acc: boolean, item: any) => acc && isMongoId(item), true)) return params.value
+    return params.value.map((item: string) => Types.ObjectId(item))
   } else {
-    if (isMongoId(params.value)) return Types.ObjectId(params.value)
-    else throw new BadRequestException(`${params.key} must be a mongodb id`)
+    if (!isMongoId(params.value)) return params.value
+    return Types.ObjectId(params.value)
   }
 }
 
 export const IsMongoIdWithTransform = (validationOptions?: ValidationOptions): PropertyDecorator => {
   return (target: any, key: string | symbol): void => {
     IsDefined(validationOptions)(target, key)
+    CustomIsMongoId(validationOptions)(target, key)
 
-    Transform(params => transformMongoIdWithParams(params, validationOptions))(target, key)
+    Transform(transformMongoIdWithParams)(target, key)
+  }
+}
+
+function CustomIsMongoId(validationOptions?: ValidationOptions) {
+  return function (object: Record<string, any>, propertyName: string | symbol) {
+    registerDecorator({
+      name: 'CustomIsMongoId',
+      target: object.constructor,
+      propertyName: propertyName.toString(),
+      options: validationOptions,
+      validator: {
+        validate(value: any): Promise<boolean> | boolean {
+          if (value instanceof Types.ObjectId) return isMongoId(value.toHexString())
+          else return isMongoId(value)
+        },
+        defaultMessage: buildMessage(eachPrefix => `${eachPrefix}$property must be a mongo id`, validationOptions),
+      },
+    })
   }
 }
--- src/modules/user/dto/user/createUser.dto.ts
+++ src/modules/user/dto/user/createUser.dto.ts
@@ -1,23 +1,68 @@
-import { IsEnum, IsNotEmpty, IsObject, IsString, MaxLength, ValidateNested } from 'class-validator'
+import { IsBoolean, IsEnum, IsNotEmpty, IsObject, IsString, MaxLength, ValidateNested } from 'class-validator'
 import { AvailabilityModel, CreateScheduleAvailabilityModel } from '../../models/user.model'
 import { Type } from 'class-transformer'
 import { ApiProperty } from '@nestjs/swagger'
 import { ClientInterfacesEnum } from '../../../../global/enums/ClientInterfaces.enum'
 import { Types } from 'mongoose'
+import { MongoIdExample, MongoIdType } from '../../../../global/constants/constants'
+import { IsUndefinable } from '../../../../global/decorators/isUndefinable.decorator'
+import { IsMongoIdWithTransform } from '../../../../global/decorators/IsMongoIdWithTransform.decorator'
 
 export class CreateScheduleAvailabilityDto implements CreateScheduleAvailabilityModel {
+  @ApiProperty({
+    description: 'Разрешено ли пользователю обращаться к этому эндпоинту',
+  })
+  @IsBoolean()
   available: boolean
 
+  @ApiProperty({
+    description:
+      'Разрешены ли пользователю для редактирования расписания всех групп. Если true, то пользователь может редактировать любое расписание. Если false, то не все и следует ориентироваться на поля availableFaculties, availableGroups, forbiddenGroups',
+  })
+  @IsBoolean()
   all: boolean
 
+  @ApiProperty({
+    description:
+      'Список доступных для редактирования факультетов. Расписания всех групп, которые относятся к этим факультетам доступны к редактированию. Важно только если в поле all стоит false',
+    type: MongoIdType,
+    example: [MongoIdExample],
+    isArray: true,
+  })
+  @IsMongoIdWithTransform({ each: true })
   availableFaculties: Types.ObjectId[]
 
+  @ApiProperty({
+    description:
+      'Список доступных для редактирования групп. Расписания этих групп доступны для редактирования. Нужно если мы хотим разрешить отдельные группы. Важно только если в поле all стоит false',
+    type: MongoIdType,
+    example: [MongoIdExample],
+    isArray: true,
+  })
+  @IsMongoIdWithTransform({ each: true })
   availableGroups: Types.ObjectId[]
 
+  @ApiProperty({
+    description:
+      'Список запрещённых для редактирования групп. Расписания этих групп запрещены для редактирования, даже если эти группы относятся к разрешённым факультетам или если они содержатся в списке разрешённых групп. Важно только если в поле all стоит false',
+    type: MongoIdType,
+    example: [MongoIdExample],
+    isArray: true,
+  })
+  @IsMongoIdWithTransform({ each: true })
   forbiddenGroups: Types.ObjectId[]
 }
 
 export class AvailabilityDto implements AvailabilityModel {
+  @ApiProperty({
+    description: 'Может ли пользователь редактировать расписание занятий',
+    type: CreateScheduleAvailabilityDto,
+    required: false,
+  })
+  @IsUndefinable()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => CreateScheduleAvailabilityDto)
   createSchedule: CreateScheduleAvailabilityDto
 }
 
