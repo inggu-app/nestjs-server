630c94bfbc0f248534eece5fd0e7816b430b4d00
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-04-04 14:46:24 +0300

    Удалена лишняя функция

--- src/global/utils/enumKeysValues.ts
+++ src/global/utils/enumKeysValues.ts
@@ -1,9 +1,3 @@
-import { objectKeys } from './objectKeys'
-
-export const getEnumValues = <T>(e: T) => {
-  return objectKeys(e).map(k => e[k])
-}
-
 export function enumKeyValuesMatch<T>(kv: { [K in keyof T]: K }) {
   return kv
 }
