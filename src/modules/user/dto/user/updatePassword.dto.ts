4f095dd4eb72ee4cba701c2b92f0521e6a10a9d8
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-04-21 17:35:23 +0300

    Обновлена модель обновления пароля пользователя

--- src/modules/user/dto/user/updatePassword.dto.ts
+++ src/modules/user/dto/user/updatePassword.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator'
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 
 export class UpdatePasswordDto {
   @ApiProperty({
     title: 'Логин пользователя, пароль которого нужно обновить.',
     example: 'admin',
+    maxLength: 60,
   })
   @IsString()
   @IsNotEmpty()
@@ -14,8 +15,11 @@ export class UpdatePasswordDto {
   @ApiProperty({
     title: 'Пароль, на который нужно обновить текущий пароль.',
     example: '123456',
+    maxLength: 30,
+    minLength: 8,
   })
   @IsString()
+  @MinLength(8)
   @MaxLength(30)
   password: string
 }
